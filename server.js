66f9e060b75a6cdf1f7839e6182cb0b5c705d912
Ernesto German Arevalo <ernesarevalo@gmail.com>
2025-05-04 02:20:09 -0300

    asdasdasdasd
    asdad

--- server.js
+++ server.js
@@ -8,21 +8,22 @@ const port = process.env.PORT || 10000;
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 // Ruta de inicio
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 // Ruta de login
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/login.html"));
+  res.sendFile(path.join(__dirname, "frontend", "login.html"));
 });
 
 // Ruta de crear rutina
 app.get("/createRoutine", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/createRoutine.html"));
+  res.sendFile(path.join(__dirname, "frontend", "createRoutine.html"));
 });
 
 // Iniciar el servidor
+const PORT = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`);
 });
